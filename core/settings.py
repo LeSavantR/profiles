8bb77b817b6593ce39adf22bc1e159f3613bb7b2
LeSavantR <ruben_hernandez1993@hotmail.es>
2022-11-17 15:08:22 -0500

    Minimal Change

--- core/settings.py
+++ core/settings.py
@@ -152,17 +152,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'prod_static')
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 STATIC_URL = 'static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'prod_media')
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'p_static')
 
-MEDIA_URL = '/media/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
